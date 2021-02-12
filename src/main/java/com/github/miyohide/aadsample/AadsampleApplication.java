bbc9da1ff563faff6a56b2bd4dbbe0bb827f466a
Hidenori Miyoshi <miyohide@gmail.com>
2021-02-12 21:00:21 +0900

    create initial files using Spring Initializr

---
+++ src/main/java/com/github/miyohide/aadsample/AadsampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.aadsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AadsampleApplication.class, args);
+    }
+
+}
