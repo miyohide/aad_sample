4e431e570ca792cdf56595e0cf885048e7d3a084
Hidenori Miyoshi <miyohide@gmail.com>
2021-02-15 20:42:20 +0900

    create JSON Utility method.

---
+++ src/main/java/com/github/miyohide/aadsample/JsonMapper.java
@@ -0,0 +1,26 @@
+package com.github.miyohide.aadsample;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+
+public class JsonMapper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+    private static final ObjectMapper MAPPER = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+
+    public static final String toJsonString(OAuth2AuthorizedClient authorizedClient) {
+        String clientJsonString = "Json String for OAuth2AuthorizedClient";
+
+        try {
+            clientJsonString = MAPPER.writeValueAsString(authorizedClient);
+        } catch (JsonProcessingException e) {
+            LOGGER.warn("Error with transfer OAuth2AuthorizedClient to Json");
+            clientJsonString = "Fail to generate Json String of current OAuth2AuthorizedClient";
+        }
+        LOGGER.info(clientJsonString);
+        return clientJsonString;
+    }
+}
